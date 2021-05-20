da1188a726961acb236a2630393a8a26b3065c87
Caroline Artz <ceartz@gmail.com>
2021-05-20 18:10:56 -0500

    Add libraries and initial render something basic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
-import './App.css'
-import logo from './logo.svg'
+import { Grommet, Text, Box, grommet } from 'grommet'
+
+import 'styled-components/macro'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet css="min-height: 100vh" theme={grommet}>
+      <Box>
+        <Text>This is some text</Text>
+      </Box>
+    </Grommet>
   )
 }
 
